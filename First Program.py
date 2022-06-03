227b2a0cc94fcc2bd03bfc775ab0ec1c4da53f7d
VarunGupta05 <106795934+VarunGupta05@users.noreply.github.com>
2022-06-03 10:50:25 +0530

    Create First Program.py
    my new program

---
+++ First Program.py
@@ -0,0 +1,10 @@
+print("we have started a project now all the developer caninclude their work time to time")
+print("when do you some modification in file please mention your name as company policy")
+lst=[1,2,3,4,5,6,7,8,9,10]
+for i in lst:
+   print(i)
+   
+print("i have done some modification")
+print("sum",sum(lst))
+print("length",len(lst))
+print("sorting",list.sort())
